e922126ce40c28f77ccd4bea16241235fab220e1
Mykyta <mykytayarokhno@gmail.com>
2023-11-18 23:00:02 +0200

    Add project files.

---
+++ RazorPagesPractice/Pages/Forms/CustomPizaa.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPractice.Pages.Forms
+{
+    public class CustomPizaaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
